27e6fea026505ee2b8a3d531046599aa87810449
Daniela Almeida <danielaspbrasil@gmail.com>
2024-04-22 11:41:28 -0300

    Feito estrutura base do site

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,16 @@
+import Banner from "componentes/Banner";
+import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+            <Banner imagem="home" />
+            <Rodape />
+        </>
+        
+    )
+}
+
+export default Inicio;
