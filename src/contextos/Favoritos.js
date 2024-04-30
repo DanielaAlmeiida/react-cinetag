b6f83603cadf519ef056db928304e37d17ed836f
Daniela Almeida <danielaspbrasil@gmail.com>
2024-04-30 11:11:41 -0300

    Criação do Hook personalizado de favoritar

--- src/contextos/Favoritos.js
+++ src/contextos/Favoritos.js
@@ -13,3 +13,26 @@ export default function FavoritosProvider({ children }) {
         </FavoritosContext.Provider>
     )
 }
+
+export function useFavoritoContext() {
+    const { favorito, setFavorito } = useContext(FavoritosContext);
+
+    function adicionarFavorito(novoFavorito) {
+        const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id)
+
+        let novaLista = [...favorito];
+
+        if (!favoritoRepetido) {
+            novaLista.push(novoFavorito);
+            return setFavorito(novaLista);
+        }
+
+        novaLista.splice(novaLista.indexOf(novoFavorito), 1);
+        return setFavorito(novaLista);
+    }
+
+    return {
+        favorito,
+        adicionarFavorito
+    }
+}
